13476c8470fa85d436020ec5935e69b01151c0c5
Kartik133 <75470483+Kartik133@users.noreply.github.com>
2021-09-28 12:57:19 +0530

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -2,14 +2,15 @@ var c,v,x;
 
 function setup() {
   createCanvas(400,400);
-  background(0);
+  
   c = new p5.AudioIn();
   c.start();
 }
 
 function draw() {
+  background(0);
   v = c.getLevel();
   fill(255);
   noStroke();
-  ellipse(200,200,200,v*20);
+  ellipse(200,200,v*200,v*200);
 }
